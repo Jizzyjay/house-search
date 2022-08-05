44311c24a7ba648d011cdec52bc81bd85d3e1cdc
Ekunola John Oluwatosin <ekunolajohn@gmail.com>
2022-08-05 12:39:52 +0100

    changes

--- src/Page/PropertyDetails.js
+++ src/Page/PropertyDetails.js
@@ -1,12 +1,7 @@
 import React from 'react';
-
-// import houseData
 import { housesData } from '../data';
-//  useParams
 import { useParams } from 'react-router-dom';
-// import icons
-import { BiBed, BiBath, BiArea, BiPhone } from 'react-icons/bi';
-// import link
+import { BiBed, BiBath, BiArea,} from 'react-icons/bi';
 import { Link } from 'react-router-dom';
 
 const PropertyDetails = () => {
